2957f9071cfd8ded694f7c2d67522547d1167825
sezen berre çelikoğlu <99209006+sezennnberre@users.noreply.github.com>
2023-02-27 16:43:54 +0300

    www.patika.dev

---
+++ patikadev-sql-odev12.sql
@@ -0,0 +1,12 @@
+SELECT COUNT(*) FROM film
+WHERE length >(SELECT AVG(length) FROM film);
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+SELECT title FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+SELECT customer_id, COUNT(customer_id) FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(customer_id) DESC;
